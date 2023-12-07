ac6c1f73e600c1283b9c801447e707cf323cf745
RinRyuu <97811148+RinRyuu@users.noreply.github.com>
2023-12-07 05:24:17 +0000

    list comprihension

---
+++ interVW/DSA/leet/string/412_Fizz Buzz.py
@@ -0,0 +1,25 @@
+'''
+https://leetcode.com/problems/fizz-buzz/
+'''
+
+n=15
+# answer=[]
+# for i in range(n):
+#     x = i+1
+#     print(i,x,x%3==0,x%5!=0)
+#     if x%3==0 and x%5!=0:
+#         answer.append('Fizz')
+#     if x%5==0 and x%3!=0:
+#         answer.append('Buzz')
+#     elif x%3==0 and x%5==0:
+#         answer.append('FizzBuzz')
+#     elif x%3!=0 and x%5!=0:
+#         answer.append(f'{i+1}')
+        
+# print(answer)
+
+#using list comprehension
+# ans= ['Fizz' * (not i%3) + 'Buzz'*(not i%5) or str(i) for i in range(1,n+1)]
+# print(ans)
+
+
