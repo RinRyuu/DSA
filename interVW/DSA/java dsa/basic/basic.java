c5fb9f22e56a2413f4d370588aa972b60b948a05
RinRyuu <97811148+RinRyuu@users.noreply.github.com>
2023-12-15 18:10:35 +0000

    started java

---
+++ interVW/DSA/java dsa/basic/basic.java
@@ -0,0 +1,6 @@
+public Class basic{
+    public static void main(String args[]){
+        Sytem.out.println("input: ")
+        
+    }
+}
---
+++ interVW/DSA/java dsa/basic/basic.py
@@ -0,0 +1,31 @@
+# a = int(input())
+# div = 2
+# flag = True
+# while div<a:
+#     if a%div==0:
+#         flag = False
+#         break
+    
+# if flag:
+#     print('yes')
+# else:
+#     print('no')
+    
+# sum=0
+# while a>0:
+#     print(a,sum)
+#     sum = a+sum
+#     a-=1
+# print(sum)
+
+# n = 9
+# while n<100:
+#     if n%2==0:
+#         print(n)
+#     n+=1
+    
+sum=0
+for i in range(1,6):
+    print(i)
+    sum = i+sum
+    print(sum/5)
