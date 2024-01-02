2682fb8d50287d794edbf1092facd49f3e7a7cf1
RinRyuu <97811148+RinRyuu@users.noreply.github.com>
2024-01-02 18:20:04 +0000

    starting java

---
+++ interVW/DSA/java dsa/basic/scans.java
@@ -0,0 +1,23 @@
+// import java.util.*;
+// public class scans {
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("give me the num for product");
+//         int a = sc.nextInt();
+//         System.out.println(a*a);
+
+//     }    
+// }
+
+import java.util.*;
+public class scans {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("give me the radius for area of circal");
+        Float r = sc.nextFloat();
+        System.out.println(3.14f*(r*r));
+        float f = 3.14f;
+        System.out.println(f);
+
+    }    
+}
